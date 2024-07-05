1d43830e2a0f00d4a656daeb694fa6ae9ec502f5
Lucas Monzon Languasco <monzonlucasfabricio@gmail.com>
2024-07-05 20:28:44 -0300

    Fixed linker script. Added init_data function to initialize the array.

--- basic/main/main.cpp
+++ basic/main/main.cpp
@@ -6,7 +6,13 @@
  */
 
 #include "esp32-hal-gpio.h"
+#include "esp_attr.h"
 #include <Arduino.h>
+#include <sys/_stdint.h>
+
+#define MYSECTION_ATTR __attribute__((section(".my_section")))
+
+MYSECTION_ATTR uint8_t mydata[128];
 
 bool estado = 0;
 
@@ -27,6 +33,15 @@ void setup() {
     xTaskCreatePinnedToCore(test, "test_task", 2048, NULL, 1, NULL, PRO_CPU_NUM);
 }
 
+void init_data(void)
+{
+	uint32_t i = 0;
+	size_t mydata_len = sizeof(mydata)/sizeof(mydata[0]);
+	for(i = 0; i < mydata_len; i++)
+	{
+		mydata[i] = i;
+	}
+}
 
 void loop() {
 	estado = !estado;
@@ -38,6 +53,7 @@ void loop() {
 extern "C" void app_main() {
     initArduino();
     setup();
+    init_data();
     while (true) {
         loop();
     }
